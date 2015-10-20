099adc5b258fb6579975000198e70d945e3fac58
Ryan Ebbers <info@ryanebbers.com>
2015-10-20 14:45:11 -0400

    removed login/register/logout from homepage nav

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,20 +7,13 @@
         <div class="title">
             Ryan Ebbers
         </div>
+
         <div class="nav">
             <a href="/about">About&nbsp;Me</a>
             <a href="/contact">Contact&nbsp;Me</a>
             <a href="/articles">Articles</a>
         </div>
 
-        <div class="nav">
-            <a href="/auth/login">Login</a>
-            <a href="/auth/register">Register</a>
-        </div>
-
-        <div class="nav">
-            <a href="/auth/logout">Logout</a>
-        </div>
     </div>
 
 @stop
