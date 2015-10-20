5f714058d3756b2c219857e9deb61a303978cdb6
Ryan Ebbers <info@ryanebbers.com>
2015-10-20 14:44:07 -0400

    created partial for user_tab

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,18 +10,7 @@
 </head>
 <body>
 
-@if ( Auth::check() )
-    <div class="user_info">
-        <h5>Welcome, {!!  Auth::user()->name !!}</h5>
-        <a href="/auth/logout">Logout</a>
-    </div>
-@else
-    <div class="user_info">
-        <h5>Welcome</h5>
-        <a href="/auth/login">Login</a><br>
-        <a href="/auth/register">Register</a>
-    </div>
-@endif
+@include('partials.user_tab')
 
 <div class="container">
 
---
+++ resources/views/partials/user_tab.blade.php
@@ -0,0 +1,12 @@
+@if ( Auth::check() )
+    <div class="user_info">
+        <h5>Welcome, {!!  Auth::user()->name !!}</h5>
+        <a href="/auth/logout">Logout</a>
+    </div>
+@else
+    <div class="user_info">
+        <h5>Welcome</h5>
+        <a href="/auth/login">Login</a><br>
+        <a href="/auth/register">Register</a>
+    </div>
+@endif
